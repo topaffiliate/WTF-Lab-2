493dc6754c83499f77d31862dc927c93d0bb9041
topaffiliate <topaffiliate1304@gmail.com>
2022-04-12 22:37:30 +0200

    lesson 7

---
+++ 610592424/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'Home/Screens/pages_screen.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: "Diploma project",
+    home: PagesScreen(),
+  ));
+}
